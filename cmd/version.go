45945729a5a7c0c6743e97e06bce4d589efe1576
B1naryB0b <35399675+B1naryB0b@users.noreply.github.com>
2025-11-01 02:52:50 +0000

    Added version cmd
    - Added versioning command
    - Version is stored in main.go

---
+++ cmd/version.go
@@ -0,0 +1,22 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var Version = "dev"
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print the version number of Wharf",
+	Long:  "Print the version number of Wharf",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("Wharf-CLI version %s\n", Version)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
